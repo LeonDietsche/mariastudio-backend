338144a1c8fb5921b1773523f9379f72c98a7dd0
LeonDietsche <dietscheleon@gmail.com>
2025-04-30 21:38:37 +0200

    Fix: add valid booking example

--- bookings.js
+++ bookings.js
@@ -1,30 +1,9 @@
 [
     {
-      "contact_appointment-dates": "2025-04-30",
-      "contact_shootdays": "",
-      "contact_email": "dietscheleon@gmail.com",
       "contact_name": "Leon Thomas Dietsche",
-      "contact_company": "",
-      "contact_phone": "+41 79 234 53 30",
-      "contact_firsttime": "no",
-      "youarea": "",
+      "contact_email": "dietscheleon@gmail.com",
       "general_project-type": "Campaign",
-      "general_photographer": "",
-      "general_brandname": "",
-      "general_magazinename": "",
-      "general_tellusmore": "",
-      "general_numberofpeople": "1to10",
-      "general_equipmentlistready": "no",
-      "general_eqlisttext": "",
-      "general_eqlistfile": {},
-      "billing_details": "same",
-      "billing_company": "",
-      "billing_street": "",
-      "billing_city": "",
-      "billing_state": "",
-      "billing_postalcode": "",
-      "billing_Country": "",
-      "billing_vatnumber": "",
+      "contact_appointment-dates": "2025-04-30",
       "date": "2025-04-30T12:00:00.000Z"
     }
   ]
